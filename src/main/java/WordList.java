dc81bbb703a4773e819cc666911c658aaec9c7ed
annierose2007 <142624392+annierose2007@users.noreply.github.com>
2024-04-15 21:52:41 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,15 +8,22 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
-  }
-
-  public void removeWordsOfLength(int len)
-  {
-    /* to be implemented in part (b) */
-
-  }
+ public int numWordsOfLength(int len) {
+        int count = 0;
+        for (String word : words) {
+            if (word.length() == len) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public void removeWordsOfLength(int len) {
+        ArrayList<String> wordsToRemove = new ArrayList<String>();
+        for (String word : words) {
+            if (word.length() == len) {
+                wordsToRemove.add(word);
+            }
+        }
+        words.removeAll(wordsToRemove);
+    }
 }
